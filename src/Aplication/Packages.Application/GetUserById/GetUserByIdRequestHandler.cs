779f61d74d405c210d47f7dd67c1936249eefe2a
Andrian <traian-007@mail.ru>
2023-04-22 20:50:41 +0300

    Configured MediatR

---
+++ src/Aplication/Packages.Application/GetUserById/GetUserByIdRequestHandler.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Packages.Application.GetUserById
+{
+    public class GetUserByIdRequestHandler : IRequestHandler<GetUserByIdRequest, GetUserByIdResponse>
+    {
+/*        public Task<GetUserByIdResponse> Handle(GetUserByIdRequest request, CancellationToken cancellationToken)
+        {
+            var user = new GetUserByIdResponse
+            {
+                Id = request.Id,
+                Name = "John"
+            };
+
+            return Task.FromResult(user);
+        }*/
+        Task<GetUserByIdResponse> IRequestHandler<GetUserByIdRequest, GetUserByIdResponse>.Handle(GetUserByIdRequest request, CancellationToken cancellationToken)
+        {
+            var user = new GetUserByIdResponse
+            {
+                Id = request.Id,
+                Name = "John"
+            };
+
+            return Task.FromResult(user);
+        }
+    }
+}
---
+++ src/Web/Packages.Api/Controllers/UsersController.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Packages.Api.Controllers.SeedWork;
+using Packages.Application.GetUserById;
+
+namespace Packages.Api.Controllers
+{
+    public class UsersController : PackagesBaseController
+    {
+        [HttpGet("{id}")] //user/id
+        public async Task<IActionResult> Get([FromRoute] int id)
+        {
+            var request = new GetUserByIdRequest { Id = id };
+            var user = await Mediator.Send(request);
+            return Ok(user);
+        }
+    }
+}
